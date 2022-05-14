d92dd7b01fabfbfa2f5aad11bcf00622cefe2019
xuhaojun <xuhaojuntw@gmail.com>
2022-05-14 22:46:47 +0800

    style: pretty

--- deps/cppjieba/Jieba.hpp
+++ deps/cppjieba/Jieba.hpp
@@ -1,29 +1,19 @@
 #ifndef CPPJIEAB_JIEBA_H
 #define CPPJIEAB_JIEBA_H
 
-#include "QuerySegment.hpp"
 #include "KeywordExtractor.hpp"
+#include "QuerySegment.hpp"
 
 namespace cppjieba {
 
 class Jieba {
- public:
-  Jieba(const string& dict_path, 
-        const string& model_path,
-        const string& user_dict_path, 
-        const string& idfPath, 
-        const string& stopWordPath) 
-    : dict_trie_(dict_path, user_dict_path),
-      model_(model_path),
-      mp_seg_(&dict_trie_),
-      hmm_seg_(&model_),
-      mix_seg_(&dict_trie_, &model_),
-      full_seg_(&dict_trie_),
-      query_seg_(&dict_trie_, &model_),
-      extractor(&dict_trie_, &model_, idfPath, stopWordPath) {
-  }
-  ~Jieba() {
-  }
+public:
+  Jieba(const string &dict_path, const string &model_path, const string &user_dict_path, const string &idfPath,
+        const string &stopWordPath)
+      : dict_trie_(dict_path, user_dict_path), model_(model_path), mp_seg_(&dict_trie_), hmm_seg_(&model_),
+        mix_seg_(&dict_trie_, &model_), full_seg_(&dict_trie_), query_seg_(&dict_trie_, &model_),
+        extractor(&dict_trie_, &model_, idfPath, stopWordPath) {}
+  ~Jieba() {}
 
   struct LocWord {
     string word;
@@ -31,58 +21,39 @@ class Jieba {
     size_t end;
   }; // struct LocWord
 
-  void Cut(const string& sentence, vector<string>& words, bool hmm = true) const {
-    mix_seg_.Cut(sentence, words, hmm);
-  }
-  void Cut(const string& sentence, vector<Word>& words, bool hmm = true) const {
-    mix_seg_.Cut(sentence, words, hmm);
-  }
-  void CutAll(const string& sentence, vector<string>& words) const {
-    full_seg_.Cut(sentence, words);
-  }
-  void CutAll(const string& sentence, vector<Word>& words) const {
-    full_seg_.Cut(sentence, words);
-  }
-  void CutForSearch(const string& sentence, vector<string>& words, bool hmm = true) const {
+  void Cut(const string &sentence, vector<string> &words, bool hmm = true) const { mix_seg_.Cut(sentence, words, hmm); }
+  void Cut(const string &sentence, vector<Word> &words, bool hmm = true) const { mix_seg_.Cut(sentence, words, hmm); }
+  void CutAll(const string &sentence, vector<string> &words) const { full_seg_.Cut(sentence, words); }
+  void CutAll(const string &sentence, vector<Word> &words) const { full_seg_.Cut(sentence, words); }
+  void CutForSearch(const string &sentence, vector<string> &words, bool hmm = true) const {
     query_seg_.Cut(sentence, words, hmm);
   }
-  void CutForSearch(const string& sentence, vector<Word>& words, bool hmm = true) const {
+  void CutForSearch(const string &sentence, vector<Word> &words, bool hmm = true) const {
     query_seg_.Cut(sentence, words, hmm);
   }
-  void CutHMM(const string& sentence, vector<string>& words) const {
-    hmm_seg_.Cut(sentence, words);
-  }
-  void CutHMM(const string& sentence, vector<Word>& words) const {
-    hmm_seg_.Cut(sentence, words);
-  }
-  void CutSmall(const string& sentence, vector<string>& words, size_t max_word_len) const {
+  void CutHMM(const string &sentence, vector<string> &words) const { hmm_seg_.Cut(sentence, words); }
+  void CutHMM(const string &sentence, vector<Word> &words) const { hmm_seg_.Cut(sentence, words); }
+  void CutSmall(const string &sentence, vector<string> &words, size_t max_word_len) const {
     mp_seg_.Cut(sentence, words, max_word_len);
   }
-  void CutSmall(const string& sentence, vector<Word>& words, size_t max_word_len) const {
+  void CutSmall(const string &sentence, vector<Word> &words, size_t max_word_len) const {
     mp_seg_.Cut(sentence, words, max_word_len);
   }
-  
-  void Tag(const string& sentence, vector<pair<string, string> >& words) const {
-    mix_seg_.Tag(sentence, words);
-  }
-  string LookupTag(const string &str) const {
-    return mix_seg_.LookupTag(str);
-  }
-  bool InsertUserWord(const string& word, const string& tag = UNKNOWN_TAG) {
+
+  void Tag(const string &sentence, vector<pair<string, string>> &words) const { mix_seg_.Tag(sentence, words); }
+  string LookupTag(const string &str) const { return mix_seg_.LookupTag(str); }
+  bool InsertUserWord(const string &word, const string &tag = UNKNOWN_TAG) {
     return dict_trie_.InsertUserWord(word, tag);
   }
 
-  bool InsertUserWord(const string& word,int freq, const string& tag = UNKNOWN_TAG) {
-    return dict_trie_.InsertUserWord(word,freq, tag);
+  bool InsertUserWord(const string &word, int freq, const string &tag = UNKNOWN_TAG) {
+    return dict_trie_.InsertUserWord(word, freq, tag);
   }
 
-  bool Find(const string& word)
-  {
-    return dict_trie_.Find(word);
-  }
+  bool Find(const string &word) { return dict_trie_.Find(word); }
 
-  void ResetSeparators(const string& s) {
-    //TODO
+  void ResetSeparators(const string &s) {
+    // TODO
     mp_seg_.ResetSeparators(s);
     hmm_seg_.ResetSeparators(s);
     mix_seg_.ResetSeparators(s);
@@ -90,30 +61,20 @@ class Jieba {
     query_seg_.ResetSeparators(s);
   }
 
-  const DictTrie* GetDictTrie() const {
-    return &dict_trie_;
-  } 
-  
-  const HMMModel* GetHMMModel() const {
-    return &model_;
-  }
+  const DictTrie *GetDictTrie() const { return &dict_trie_; }
 
-  void LoadUserDict(const vector<string>& buf)  {
-    dict_trie_.LoadUserDict(buf);
-  }
+  const HMMModel *GetHMMModel() const { return &model_; }
 
-  void LoadUserDict(const set<string>& buf)  {
-    dict_trie_.LoadUserDict(buf);
-  }
+  void LoadUserDict(const vector<string> &buf) { dict_trie_.LoadUserDict(buf); }
 
-  void LoadUserDict(const string& path)  {
-    dict_trie_.LoadUserDict(path);
-  }
+  void LoadUserDict(const set<string> &buf) { dict_trie_.LoadUserDict(buf); }
 
- private:
+  void LoadUserDict(const string &path) { dict_trie_.LoadUserDict(path); }
+
+private:
   DictTrie dict_trie_;
   HMMModel model_;
-  
+
   // They share the same dict trie and model
   MPSegment mp_seg_;
   HMMSegment hmm_seg_;
@@ -121,7 +82,7 @@ class Jieba {
   FullSegment full_seg_;
   QuerySegment query_seg_;
 
- public:
+public:
   KeywordExtractor extractor;
 }; // class Jieba
 
